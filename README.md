3f3e128fc8a0fc6cf875cd90d4641d6e00539555
Elvin Shrestha <elwyncrestha@gmail.com>
2020-06-12 09:02:30 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WebCare(A Web Portal for Health Centre Management)
+
+* Web Application (Progressive Web Application)
+* Technology Stack - Java (Spring Framework) and Angular 9
---
+++ pemits-web/src/test/java/com/pemits/webcare/WebCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pemits.webcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
