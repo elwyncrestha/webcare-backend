c585ba09e02f5333bbb0746c9d9145369a20a11a
Prerana Pandit <preranapandit07@gmail.com>
2020-06-29 09:06:17 +0545

    Test: Added test cases for UserRepository

--- pemits-api/src/test/java/com/pemits/webcare/api/user/repository/UserRepositoryTest.java
+++ pemits-api/src/test/java/com/pemits/webcare/api/user/repository/UserRepositoryTest.java
@@ -2,8 +2,11 @@ package com.pemits.webcare.api.user.repository;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -22,6 +25,43 @@ public class UserRepositoryTest extends BaseJpaTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Test
+    public void testSaveShouldReturnSavedUser() {
+        User user = new User();
+        user.setName("John Doe");
+        user.setEmail("john@mail.com");
+        user.setUsername("johndoe");
+        user.setUserType(UserType.ADMINISTRATOR);
+        user.setPassword("123456");
+
+        User saved = userRepository.save(user);
+
+        assertThat(saved.getId(), notNullValue());
+    }
+
+    @Test
+    public void testSaveAllShouldReturnAllSavedUser() {
+        User user1 = new User();
+        user1.setName("John Doe");
+        user1.setEmail("john@mail.com");
+        user1.setUsername("johndoe");
+        user1.setUserType(UserType.ADMINISTRATOR);
+        user1.setPassword("123456");
+
+        User user2 = new User();
+        user2.setName("Jane Doe");
+        user2.setEmail("jane@mail.com");
+        user2.setUsername("janedoe");
+        user2.setUserType(UserType.ADMINISTRATOR);
+        user2.setPassword("987654");
+
+        List<User> saved = userRepository.saveAll(Arrays.asList(user1, user2));
+
+        assertThat(saved, hasSize(2));
+        assertThat(saved.get(0).getId(), notNullValue());
+        assertThat(saved.get(1).getId(), notNullValue());
+    }
 
     @Test
     @DatabaseSetup("/dataset/user/admin-config.xml")
@@ -45,5 +85,31 @@ public class UserRepositoryTest extends BaseJpaTest {
         final User user = userRepository.getOne(DEFAULT_ADMIN_ID);
 
         assertThat(user.getUserType(), is(UserType.SUPER_ADMINISTRATOR));
+    }
+
+    @Test
+    @DatabaseSetup("/dataset/user/admin-config.xml")
+    public void testDeleteByIdShouldDeleteAdmin() {
+        userRepository.deleteById(DEFAULT_ADMIN_ID);
+
+        assertThat(userRepository.findAll(), hasSize(0));
+    }
+
+    @Test
+    @DatabaseSetup("/dataset/user/admin-config.xml")
+    public void testDeleteShouldDeleteAdmin() {
+        final User user = userRepository.getOne(DEFAULT_ADMIN_ID);
+
+        userRepository.delete(user);
+
+        assertThat(userRepository.findAll(), hasSize(0));
+    }
+
+    @Test
+    @DatabaseSetup("/dataset/user/admin-config.xml")
+    public void testDeleteAllShouldReturnCountZero() {
+        userRepository.deleteAll();
+
+        assertThat(userRepository.findAll(), hasSize(0));
     }
 }
