18558835bcce44c50da86cdfd399589a2b614f00
Ishika Pradhan <isu.pradhan@gmail.com>
2020-08-09 10:56:06 +0545

    Feature: Added Inventory Filter

--- pemits-api/src/main/java/com/pemits/webcare/api/inventory/repository/spec/InventorySpec.java
+++ pemits-api/src/main/java/com/pemits/webcare/api/inventory/repository/spec/InventorySpec.java
@@ -1,18 +1,21 @@
 package com.pemits.webcare.api.inventory.repository.spec;
 
-import com.pemits.webcare.api.inventory.entity.Inventory;
-import org.springframework.data.jpa.domain.Specification;
-
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.pemits.webcare.api.inventory.entity.Inventory;
 
 /**
  * @Author Mohammad Hussain
  * created on 6/29/2020
  */
 public class InventorySpec implements Specification<Inventory> {
+
+    private static final String FILTER_BY_NAME = "name";
 
     private final String property;
     private final String value;
@@ -25,7 +28,12 @@ public class InventorySpec implements Specification<Inventory> {
 
     @Override
     public Predicate toPredicate(Root<Inventory> root, CriteriaQuery<?> query,
-                                 CriteriaBuilder criteriaBuilder) {
-        return null;
+        CriteriaBuilder criteriaBuilder) {
+        switch (property) {
+            case FILTER_BY_NAME:
+                return criteriaBuilder.like(root.get(FILTER_BY_NAME), "%" + value + "%");
+            default:
+                return null;
+        }
     }
 }
